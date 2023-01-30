649e343ac2ba0ef57e54085408f25ee3e3b543e0
denis daros <denisv.daros@gmail.com>
2023-01-29 21:57:06 -0300

    organizando ambiente

---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Users extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+    },
+    username: {
+      type: DataTypes.STRING,
+    },
+    role: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'Users',
+    timestamps: false,
+  },
+);
+
+export default Users;
