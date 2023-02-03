f5351c276426bdeb8434294f7e76d9bca782bcd7
denis daros <denisv.daros@gmail.com>
2023-02-02 21:03:34 -0300

    requisito 20

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -3,9 +3,16 @@ import matchesService from '../services/matches.services';
 
 class matchesController {
   static async allmatches(req: Request, res: Response, next: NextFunction) {
-    const matches = await matchesService.allmatches();
-    if (!matches) return res.status(200).json({ message: 'deu ruim' });
-    res.status(200).json(matches);
+    const { inProgress } = req.query;
+
+    if (inProgress === 'true') {
+      const matches = await matchesService.allmatchesProgress();
+      res.status(200).json(matches);
+    } else {
+      const matches = await matchesService.allmatches();
+      res.status(200).json(matches);
+    }
+
     next();
   }
 }
--- app/backend/src/services/matches.services.ts
+++ app/backend/src/services/matches.services.ts
@@ -16,6 +16,21 @@ class matchesService {
 
     return matches;
   }
+
+  static async allmatchesProgress() {
+    const matches = await Match.findAll({ where: { inProgress: true },
+      include: [
+        {
+          model: Team, as: 'homeTeam', attributes: { exclude: ['id'] },
+        },
+        {
+          model: Team, as: 'awayTeam', attributes: { exclude: ['id'] },
+        },
+      ],
+    });
+
+    return matches;
+  }
 }
 
 export default matchesService;
